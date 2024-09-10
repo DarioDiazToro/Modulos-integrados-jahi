5b59f34a11bf21dbebe662fedd8178a501644406
DarioDiaz <haiverdiaztoro@gmail.com>
2024-09-10 12:31:26 -0500

    index.js añadido en public

---
+++ src/public/js/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const messageElement = document.getElementById('message');
+
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        try {
+            const response = await fetch('localhost:3000/api/v1/login', { // Cambia la ruta a tu endpoint de autenticación
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: username, password }), // Asegúrate de que coincida con los datos que espera tu API
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                messageElement.textContent = 'Inicio de sesión exitoso';
+                messageElement.style.color = 'green';
+                localStorage.setItem('token', result.token); // Guardar el token para futuras solicitudes
+                // Redirigir o realizar alguna acción adicional
+            } else {
+                messageElement.textContent = result.msg;
+                messageElement.style.color = 'red';
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            messageElement.textContent = 'Error de conexión con el servidor';
+            messageElement.style.color = 'red';
+        }
+    });
+});
